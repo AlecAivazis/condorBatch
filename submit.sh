901ac6be509d1f2b05327ccaa2a8a7ebfb8353c8
Alec Aivazis <alec@aivazis.com>
2014-07-22 10:27:18 -0700

    submit script no longer zips up the target file but just send it along

--- submit.sh
+++ submit.sh
@@ -3,8 +3,7 @@
 # author: alec aivazis
 
 # the directory to loop over
-dir=/hadoop/cms/store/user/aaivazis/samples/signal/raw/200
-
+dir=/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-23
 # keep track of the batch counter
 batch=0
 # store the directory that we start in so we can move files back
@@ -20,9 +19,9 @@ fi
 
 for f in ${dir}/*.root
 do
-  echo "[ compressing target root file... ]"
-  echo ${f}
-  tar -zcf source-${batch}.tar.gz -C ${dir} $(basename ${f})
+  #echo "[ compressing target root file... ]"
+  #echo ${f}
+  #tar -zcf source-${batch}.tar.gz -C ${dir} $(basename ${f})
 
   echo "[ creating condor submission file... ]"
   rm -rf batch_condor
@@ -33,7 +32,7 @@ Requirements = Memory >= 199 && OpSys == "LINUX"&& (Arch != "DUMMY" )&& Disk > 1
 Should_Transfer_Files = YES
 +TransferOutput = ""
 WhenToTransferOutput = ON_EXIT
-Transfer_Input_Files = include.tar.gz, source-${batch}.tar.gz
+Transfer_Input_Files = ${f}, include.tar.gz
 Output = logs/out_${batch}
 Error = logs/error_${batch}
 Log = logs/log_${batch}
